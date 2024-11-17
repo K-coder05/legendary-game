a80f5ef329357154aa08c315256d4bec35f5d20d
bvuong19 <vuongbrian3@gmail.com>
2024-11-17 10:10:24 -0800

    added control screen assets

--- screen_change.py
+++ screen_change.py
@@ -8,6 +8,51 @@ pause_img = pygame.image.load("Assets/MenuButtons/PauseButton.png").convert_alph
 quit_img = pygame.image.load("Assets/MenuButtons/QuitButton.png").convert_alpha()
     
 
+def controls_screen():
+    # Load control images
+    key_left_img = pygame.image.load("Assets/MenuButtons/KeyboardLeft.png").convert_alpha()
+    key_right_img = pygame.image.load("Assets/MenuButtons/KeyboardRight.png").convert_alpha()
+    key_up_img = pygame.image.load("Assets/MenuButtons/KeyboardUp.png").convert_alpha()
+    key_down_img = pygame.image.load("Assets/MenuButtons/KeyboardDown.png").convert_alpha()
+    key_space1_img = pygame.image.load("Assets/MenuButtons/KeyboardSpace1.png").convert_alpha()
+    key_space2_img = pygame.image.load("Assets/MenuButtons/KeyboardSpace2.png").convert_alpha()
+    key_space3_img = pygame.image.load("Assets/MenuButtons/KeyboardSpace3.png").convert_alpha()
+    key_add_image = pygame.image.load("Assets/MenuButtons/KeyboardA.png").convert_alpha()
+    key_sub_image = pygame.image.load("Assets/MenuButtons/KeyboardS.png").convert_alpha()
+
+    # 1100 x 700 (550 x 350) window dimensions
+
+    controls_run = True
+    while controls_run:
+        main.WIN.fill((0, 0, 0))
+
+        main.draw_text("CONTROLS", main.FONT, (255, 255, 255), 0, -200)
+
+        main.draw_text("Use the arrow keys to move", main.FONT, (255, 255, 255), 0, -100)
+        main.WIN.blit(key_left_img, (main.WIDTH/2 - 100, main.HEIGHT/2 - 50))
+        main.WIN.blit(key_right_img, (main.WIDTH/2 - 50, main.HEIGHT/2 - 50))
+        main.WIN.blit(key_up_img, (main.WIDTH/2 + 18, main.HEIGHT/2 - 50))
+        main.WIN.blit(key_down_img, (main.WIDTH/2 + 68, main.HEIGHT/2 - 50))
+
+        main.draw_text("Press 'Space' to pause the game", main.FONT, (255, 255, 255), 0, 50)
+        main.WIN.blit(key_space1_img, (main.WIDTH/2 - 48, main.HEIGHT/2 + 100))
+        main.WIN.blit(key_space2_img, (main.WIDTH/2 - 15, main.HEIGHT/2 + 100))
+        main.WIN.blit(key_space3_img, (main.WIDTH/2 + 16, main.HEIGHT/2 + 100))
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    controls_run = True
+                    return True
+            if event.type == pygame.QUIT:
+                controls_run = False
+                return False
+
+
+        pygame.display.update()
+
+
 def lose_screen():
     play_button = button.Button(main.WIDTH/2 - 80, main.HEIGHT/2 + 50, play_img, 1.5)
     quit_button = button.Button(main.WIDTH/2 - 75, main.HEIGHT/2 + 150, quit_img, 1.5)
@@ -81,6 +126,10 @@ def pause_screen():
             if event.type == pygame.QUIT:
                 game_paused = False
                 return False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    game_paused = True
+                    return True
 
         pygame.display.update()
 
