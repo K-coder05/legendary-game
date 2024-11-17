bca2b3ebb652ec0a460780d65677273b56d84a41
Brian Vuong <bvuong@Brians-MacBook-Air.local>
2024-11-16 16:13:25 -0800

    Final touches finished on the pause menu and loss menu

--- screen_change.py
+++ screen_change.py
@@ -2,13 +2,27 @@ import pygame
 import main
 
 def lose_screen():
-    main.WIN.fill((0, 0, 0))
-    main.draw_text("You lost, better luck next time", main.FONT, (255, 255, 255), 150, 250)
-    main.run = False
+    lose_run = True
+    while lose_run:
+        main.WIN.fill((0, 0, 0))
+        main.draw_text("You lost, better luck next time", main.FONT, (255, 255, 255), 150, 250)
+        main.draw_text("Press 'Space' to try again", main.FONT, (255, 255, 255), 150, 350)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    lose_run = False
+                    return True
+            if event.type == pygame.QUIT:
+                lose_run = False
+                return False
+            
+        pygame.display.update()
+
 
 def main_screen():
-    run = True
-    while run:
+    main_run = True
+    while main_run:
         main.WIN.fill((0, 0, 0))
         main.draw_text("Welcome to Legendary Game", main.FONT, (255, 255, 255), 200, 250)
         main.draw_text("Press 'Space' to start", main.FONT, (255, 255, 255), 250, 300)
@@ -16,13 +30,54 @@ def main_screen():
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
+                    main_run = False
                     return True
             if event.type == pygame.QUIT:
+                main_run = False
                 return False
             
         pygame.display.update()
 
+
+def pause_screen():
+    game_paused = True
+    while game_paused:
+        # Draw the pause screen
+        main.WIN.fill((0, 0, 0))
+        main.draw_text("Press 'Space' to Resume", main.FONT, (255, 255, 255), 200, 250)
+        main.draw_text("Press 'Escape' to exit", main.FONT, (255, 255, 255), 250, 300)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_paused = False
+                return False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    game_paused = False
+                    return True
+                if event.key == pygame.K_ESCAPE:
+                    game_paused = False
+                    main.run = False
+    
+
+        pygame.display.update()
+
+
 def win_screen():
-    main.WIN.fill((0, 0, 0))
-    main.draw_text("Congratuations!", main.FONT, (255, 255, 255), 200, 250)
-    main.run = False
+    win_run = True
+    while win_run:
+        main.WIN.fill((0, 0, 0))
+        main.draw_text("Congratuations!", main.FONT, (255, 255, 255), 200, 250)
+        main.draw_text("Press 'Space' to try again", main.FONT, (255, 255, 255), 150, 350)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    win_run = False
+                    return True
+            if event.type == pygame.QUIT:
+                win_run = False
+                return False
+            
+        pygame.display.update()
+        
